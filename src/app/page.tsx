954d8074b0f250bda9322d67a4dfa809a528c21f
Arjun-Daniel <arjunndaniell@gmail.com>
2024-10-04 19:55:35 +0530

    error fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ const handleLogin = (e: React.FormEvent) => {
 
   const storedUser = localStorage.getItem(email);
     if (!storedUser) {
-      setErrorMessage("Email ID not detected. Kindly Sign Up for an account.");
+      setErrorMessage("Email ID not detected. Please try again.");
       return;
     }
 
@@ -26,7 +26,7 @@ const handleLogin = (e: React.FormEvent) => {
     if (parsedUser.password !== password) {
       setErrorMessage("Invalid password, please try again.");
     } else {
-      setSuccessMessage("Email Account Created, and Successfully Signed In!");
+      setSuccessMessage("Successfully Signed In!");
       setErrorMessage("");
 
       setEmail("");
@@ -40,13 +40,13 @@ const handleLogin = (e: React.FormEvent) => {
         <aside className="bg-white w-full max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl rounded-lg bg-opacity-60 shadow-lg shadow-orange-500">
         <h1 className="text-center text-2xl sm:text-3xl font-bold bg-orange-500 text-black rounded-t-lg m-0 py-3 sm:py-4">Sign In</h1>
           <form onSubmit={handleLogin} className="p-6 sm:p-8">
-            <input type=" text" name="" placeholder="Email id" value={email} onFocus={clearErrorMessage} onChange={(e) => setEmail(e.target.value)} className="py-2 px-3 w-full text-black text-lg font-light outline-none border border-black "/>
-            <input type="text" name="" placeholder="Password" value={password} onFocus={clearErrorMessage} onChange={(e) => setPassword(e.target.value)} className="py-2 px-3 w-full text-black text-lg font-light outline-none border border-black mt-4 sm:mt-5" />
+            <input type="email" name="" placeholder="Email id" value={email} onFocus={clearErrorMessage} onChange={(e) => setEmail(e.target.value)} className="py-2 px-3 w-full text-black text-lg font-light outline-none border border-black "/>
+            <input type="password" name="" placeholder="Password" value={password} onFocus={clearErrorMessage} onChange={(e) => setPassword(e.target.value)} className="py-2 px-3 w-full text-black text-lg font-light outline-none border border-black mt-4 sm:mt-5" />
             {errorMessage && <p className="text-white text-center mt-3 border border-red-600 bg-red-700 rounded-lg font-bold m-0 py-3 sm:py-4">{errorMessage}</p>}
             {successMessage && <p className="text-white text-center mt-3 border border-red-600 bg-blue-600 rounded-lg font-bold m-0 py-3 sm:py-4">{successMessage}</p>}
               <div className="flex justify-between items-center mt-4 sm:mt-5">
                 <Link href="/signup" className="text-black cursor-pointer transition hover:text-orange-600">Create an account</Link>
-                <button type="submit" className="py-2 px-4 sm:px-8 bg-black text-white rounded-lg transition hover:bg-orange-600">Sing In</button>
+                <button type="submit" className="py-2 px-4 sm:px-8 bg-black text-white rounded-lg transition hover:bg-orange-600">Sign In</button>
               </div>
           </form>
         </aside>
